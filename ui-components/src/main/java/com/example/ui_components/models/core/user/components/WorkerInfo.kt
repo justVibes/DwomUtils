f5c3a5aa283101976fab25710d0199dcf6d0a216
justVibes <wavyevybz@gmail.com>
2024-11-05 17:52:13 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/models/core/user/components/WorkerInfo.kt
+++ ui-components/src/main/java/com/example/ui_components/models/core/user/components/WorkerInfo.kt
@@ -1,8 +1,6 @@
 package com.example.ui_components.models.core.user.components
 
-import com.google.firebase.firestore.CollectionReference
-
 data class WorkerInfo(
     var formattedWorkerTitle: String,
-    var establishmentRef: CollectionReference,
+    var establishmentPath: String,
 )
