4998a77a4d51bcb7de955e295f1320b1475d3868
justVibes <wavyevybz@gmail.com>
2024-11-05 18:32:44 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/models/core/user/components/WorkerInfo.kt
+++ ui-components/src/main/java/com/example/ui_components/models/core/user/components/WorkerInfo.kt
@@ -1,6 +1,6 @@
 package com.example.ui_components.models.core.user.components
 
 data class WorkerInfo(
-    var formattedWorkerTitle: String,
-    var establishmentPath: String,
+    var formattedWorkerTitle: String = "",
+    var establishmentPath: String = "",
 )
