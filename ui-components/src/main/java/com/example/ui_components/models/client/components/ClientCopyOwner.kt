516570f1d9bdb07e57da4ff1120945d309bbf497
justVibes <wavyevybz@gmail.com>
2024-11-09 17:51:50 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/models/client/components/ClientCopyOwner.kt
+++ ui-components/src/main/java/com/example/ui_components/models/client/components/ClientCopyOwner.kt
@@ -3,6 +3,6 @@ package com.example.ui_components.models.client.components
 import com.google.firebase.firestore.DocumentReference
 
 data class ClientCopyOwner (
-    val email: String = "",
-    val docRef: DocumentReference? = null
+    val ownerInfoDocRef: DocumentReference? = null,
+    val clientCopyDocRef: DocumentReference? = null
 )
