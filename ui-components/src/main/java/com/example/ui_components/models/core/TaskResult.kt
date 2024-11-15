c400ef019bd6ef369360761eb85e3f828eb7f09f
justVibes <wavyevybz@gmail.com>
2024-11-14 19:55:13 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/models/core/TaskResult.kt
+++ ui-components/src/main/java/com/example/ui_components/models/core/TaskResult.kt
@@ -1,6 +1,7 @@
 package com.example.ui_components.models.core
 
+
 sealed class TaskResult {
-    data class SUCCESS<T>(val data: T, val message: String = "") : TaskResult()
-    data class FAIL(val message: String): TaskResult()
+    data class SUCCESS(val data: Any? = null, val message: String = "") : TaskResult()
+    data class FAIL(val message: String) : TaskResult()
 }
