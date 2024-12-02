3c4bba4da9ec230d2f1b20cb85ea9a06ada9078d
justVibes <wavyevybz@gmail.com>
2024-12-01 22:56:07 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/models/ClientFile.kt
+++ ui-components/src/main/java/com/example/ui_components/models/ClientFile.kt
@@ -4,6 +4,6 @@ import com.example.ui_components.models.client.ClientItem
 
 data class ClientFile(
     /* Use the names from the 'ClientDetailsDestinations' enum class to initialize */
-    val accessibleSections: Set<String> = emptySet(),
+    val accessibleSections: List<String> = listOf(),
     val client: ClientItem = ClientItem()
 )
