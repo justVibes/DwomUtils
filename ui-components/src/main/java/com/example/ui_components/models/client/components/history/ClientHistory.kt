435371d1c597b4405b8d0ff886c42f0240ac2360
justVibes <wavyevybz@gmail.com>
2024-12-07 21:18:51 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/models/client/components/history/ClientHistory.kt
+++ ui-components/src/main/java/com/example/ui_components/models/client/components/history/ClientHistory.kt
@@ -1,11 +1,16 @@
 package com.example.ui_components.models.client.components.history
 
+import com.example.ui_components.models.client.ClientItem
 import com.example.ui_components.models.client.components.emergency_contact_info.EmergencyContactInfo
 import com.example.ui_components.models.client.components.history.variants.LocalClientHistory
 import com.example.ui_components.models.client.components.info.ClientInfo
+import com.example.ui_components.models.client.components.lab_result.LabResult
+import com.example.ui_components.models.client.components.note.ClientNote
 import com.example.ui_components.models.client.components.service_provider.ServiceProvider
 import com.example.ui_components.models.client.components.vitals.ClientVitals
+import io.realm.kotlin.ext.toRealmList
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 data class ClientHistory(
@@ -14,6 +19,8 @@ data class ClientHistory(
     val clientInfo: ClientInfo = ClientInfo(),
     val vitals: ClientVitals = ClientVitals(),
     val emergencyContactInfo: EmergencyContactInfo = EmergencyContactInfo(),
+    val notes: List<ClientNote> = emptyList(),
+    @Transient val labResults: List<LabResult> = emptyList()
 ) {
     object Config {
         fun mapToLocal(form: ClientHistory) = LocalClientHistory().apply {
@@ -22,6 +29,17 @@ data class ClientHistory(
             clientInfo = ClientInfo.Config.mapToLocal(form.clientInfo)
             vitals = ClientVitals.Config.mapToLocal(form.vitals)
             emergencyContactInfo = EmergencyContactInfo.Config.mapToLocal(form.emergencyContactInfo)
+            notes = form.notes.map { ClientNote.Config.mapToLocal(it) }.toRealmList()
+            labResults = form.labResults.map { LabResult.Config.mapToLocal(it) }.toRealmList()
         }
+
+        fun mapToClientItem(form: ClientHistory) = ClientItem(
+            serviceProvider = form.serviceProvider,
+            clientInfo = form.clientInfo,
+            vitals = form.vitals,
+            emergencyContactInfo = form.emergencyContactInfo,
+            tempNotes = form.notes,
+            labResults = form.labResults
+        )
     }
 }
--- ui-components/src/main/java/com/example/ui_components/models/client/components/history/variants/LocalClientHistory.kt
+++ ui-components/src/main/java/com/example/ui_components/models/client/components/history/variants/LocalClientHistory.kt
@@ -3,9 +3,16 @@ package com.example.ui_components.models.client.components.history.variants
 import com.example.ui_components.models.client.components.emergency_contact_info.variants.LocalEmergencyContactInfo
 import com.example.ui_components.models.client.components.history.ClientHistory
 import com.example.ui_components.models.client.components.info.variants.LocalClientInfo
+import com.example.ui_components.models.client.components.lab_result.LabResult
+import com.example.ui_components.models.client.components.lab_result.variants.LocalLabResult
+import com.example.ui_components.models.client.components.note.ClientNote
+import com.example.ui_components.models.client.components.note.variants.LocalClientNote
 import com.example.ui_components.models.client.components.service_provider.LocalServiceProvider
 import com.example.ui_components.models.client.components.vitals.variants.LocalClientVitals
+import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.types.EmbeddedRealmObject
+import io.realm.kotlin.types.RealmList
+import kotlinx.serialization.Transient
 
 class LocalClientHistory : EmbeddedRealmObject {
     var datePosted: Long = 0L
@@ -13,6 +20,8 @@ class LocalClientHistory : EmbeddedRealmObject {
     var clientInfo: LocalClientInfo? = null
     var vitals: LocalClientVitals? = null
     var emergencyContactInfo: LocalEmergencyContactInfo? = null
+    var notes: RealmList<LocalClientNote> = realmListOf()
+    var labResults: RealmList<LocalLabResult> = realmListOf()
 
     object Config {
         fun mapToOriginal(form: LocalClientHistory) = ClientHistory(
@@ -24,7 +33,9 @@ class LocalClientHistory : EmbeddedRealmObject {
             vitals = LocalClientVitals.Config.mapToOriginal(form.vitals ?: LocalClientVitals()),
             emergencyContactInfo = LocalEmergencyContactInfo.Config.mapToOriginal(
                 form.emergencyContactInfo ?: LocalEmergencyContactInfo()
-            )
+            ),
+            notes = form.notes.map { LocalClientNote.Config.mapToOriginal(it) },
+            labResults = form.labResults.map { LocalLabResult.Config.mapToOriginal(it) }
         )
     }
 }
