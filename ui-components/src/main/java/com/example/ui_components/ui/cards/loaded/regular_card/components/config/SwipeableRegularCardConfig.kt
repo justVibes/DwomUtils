bcb9c5a2d768793a81ea02e6e3bbf0ede673ed0f
justVibes <wavyevybz@gmail.com>
2024-12-30 19:28:42 -0500

    Update

--- ui-components/src/main/java/com/example/ui_components/ui/cards/loaded/regular_card/components/config/SwipeableRegularCardConfig.kt
+++ ui-components/src/main/java/com/example/ui_components/ui/cards/loaded/regular_card/components/config/SwipeableRegularCardConfig.kt
@@ -3,7 +3,7 @@ package com.example.ui_components.ui.cards.loaded.regular_card.components.config
 import androidx.compose.material3.SwipeToDismissBoxValue
 import com.example.ui_components.ui.cards.loaded.regular_card.components.SwipeSide
 
-data class SwipeableRegularCardConfig(
+data class SwipeableRegularCardConfig<T: Any?>(
     val swipeSides: Pair<SwipeSide, SwipeSide>,
-    val onSwipe: (SwipeToDismissBoxValue) -> Boolean
+    val onSwipe: (SwipeToDismissBoxValue, T) -> Boolean
 )
